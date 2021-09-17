6a98271efde37ac8b51eaf9ac659ab9ede61a028
Tatiana Kikalova <t_kikalova@bk.ru>
2021-09-17 17:31:47 +0200

    Changes were made according to the comments from krglkvrmn: https://github.com/Tatiana-kik/BI_2021_Python/pull/1

--- HomeWork1.py
+++ HomeWork1.py
@@ -2,31 +2,25 @@ mapping_DNA = {'G': 'C', 'C': 'G', 'T': 'A', 'A': 'T', 'g': 'c', 'c': 'g', 't': 
 mapping_RNA = {'G': 'C', 'C': 'G', 'U': 'A', 'A': 'U', 'g': 'c', 'c': 'g', 'u': 'a', 'a': 'u'}
 
 def check_sequence(sequence):
-    trust = True
-    for nucleotide in sequence:
-        if not (nucleotide in mapping_DNA.keys()):
-            trust = False
+    trust = set(sequence) <= set(mapping_DNA.keys())
     if trust:
         return True, 'DNA'
-    trust = True
-    for nucleotide in sequence:
-        if not (nucleotide in mapping_RNA.keys()):
-            trust = False
+    trust = set(sequence) <= set(mapping_RNA.keys())
     if trust:
         return True, 'RNA'
     else:
         return False, ''
 
 def transcribe(sequence, type):
-    result = ''
+    result = []
     for nucleotide in sequence:
         if nucleotide == 'T':
-            result = result + 'U'
+            result.append('U')
         elif nucleotide == 't':
-            result = result + 'u'
+            result.append('u')
         else:
-            result = result + nucleotide
-    return result
+            result.append(nucleotide)
+    return "".join(result)
 
 def complement(sequence, type):
     mapping = {'DNA': mapping_DNA, 'RNA': mapping_RNA}
@@ -35,13 +29,29 @@ def complement(sequence, type):
         result = result + mapping[type][nucleotide]
     return result
 
-def reverse(sequence):
+def reverse(sequence, type):
     return sequence[::-1]
 
 def reverse_complement(sequence, type):
-    reversed_sequence = reverse(sequence)
+    reversed_sequence = reverse(sequence, type)
     reversed_complemented = complement(reversed_sequence, type)
     return reversed_complemented
+
+def do_command(command):
+    action = {
+        'reverse': reverse,
+        'complement': complement,
+        'reverse complement': reverse_complement
+    }
+    correctness = False
+    while not correctness:
+        enter_sequence = input("Please enter sequence you want to " + command + ": \n")
+        correct, type_sequence = check_sequence(enter_sequence)
+        if not correct:
+            print('Wrong alphabet! Try again:')
+        else:
+            print(action[command](enter_sequence, type_sequence))
+            correctness = True
 
 if __name__ == "__main__":
     while True:
@@ -68,32 +78,8 @@ if __name__ == "__main__":
                 else:
                     print('Houston, we have a problem!')
         elif inserted_command == 'reverse':
-            correctness = False
-            while not correctness:
-                enter_sequence = input('Please enter sequence you want to reverse:\n')
-                correct, type_sequence = check_sequence(enter_sequence)
-                if not correct:
-                    print('Wrong alphabet! Try again:')
-                else:
-                    print(reverse(enter_sequence))
-                    correctness = True
+            do_command('reverse')
         elif inserted_command == 'complement':
-            correctness = False
-            while not correctness:
-                enter_sequence = input('Please enter sequence you want to complement:\n')
-                correct, type_sequence = check_sequence(enter_sequence)
-                if not correct:
-                    print('Wrong alphabet! Try again:')
-                else:
-                    print(complement(enter_sequence, type_sequence))
-                    correctness = True
+            do_command('complement')
         elif inserted_command == 'reverse complement':
-            correctness = False
-            while not correctness:
-                enter_sequence = input('Please enter sequence you want to reverse complement:\n')
-                correct, type_sequence = check_sequence(enter_sequence)
-                if not correct:
-                    print('Wrong alphabet! Try again:')
-                else:
-                    print(reverse_complement(enter_sequence, type_sequence))
-                    correctness = True
+            do_command('reverse complement')
